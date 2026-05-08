7e445b93ea23401e5d97d955b6c1d9ef533da9c7
Hudhali <52822865+A-A-J@users.noreply.github.com>
2026-05-08 03:32:28 +0300

    Fix index dashboard data rendering

--- html/modal/menu/menu.js
+++ html/modal/menu/menu.js
@@ -1,13 +1,20 @@
-import { formatMoney, setText } from '../functions/format.js';
+import { formatMoney } from '../functions/format.js';
 
 export function loadMenuStats(data) {
-    const cards = document.querySelectorAll('.card h2');
+    document.getElementById('balanceValue').innerText = formatMoney(data.stats.balance);
+    document.getElementById('dailyRevenueValue').innerText = formatMoney(data.stats.dailyRevenue);
+    document.getElementById('employeesValue').innerText = data.stats.employees;
+    document.getElementById('onlineValue').innerText = 'Online: ' + data.stats.onlineEmployees;
+    document.getElementById('vehiclesValue').innerText = data.stats.vehicles;
+    document.getElementById('garageValue').innerText = 'In Garage: ' + data.stats.garageVehicles;
 
-    cards[0].innerText = formatMoney(data.stats.balance);
-    cards[1].innerText = data.stats.employees;
-    cards[2].innerText = data.stats.vehicles;
+    const jobInfo = document.getElementById('jobInfo');
 
-    setText('.info-row:nth-child(2) strong', data.job.name);
-    setText('.info-row:nth-child(3) strong', data.job.grade);
-    setText('.info-row:nth-child(4) strong', data.job.members);
+    jobInfo.innerHTML = `
+        <div class="info-row"><span>Job Name</span><strong>${data.job.name}</strong></div>
+        <div class="info-row"><span>Job Label</span><strong>${data.job.label}</strong></div>
+        <div class="info-row"><span>Grade</span><strong>${data.job.grade}</strong></div>
+        <div class="info-row"><span>Members</span><strong>${data.job.members}</strong></div>
+        <div class="info-row"><span>Created</span><strong>${data.job.created}</strong></div>
+    `;
 }
