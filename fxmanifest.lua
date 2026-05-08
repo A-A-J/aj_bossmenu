b629264e2d6496aa30d4ad14a151bf53f2cd660a
Hudhali <52822865+A-A-J@users.noreply.github.com>
2026-05-08 03:06:10 +0300

    Add base FiveM bossmenu resource

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'aj_bossmenu'
+author 'A-A-J'
+description 'Modern FiveM NUI Boss Menu'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
